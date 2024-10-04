99973917cc4e6b7c5410b0e8d1de7f6e1abdc1aa
pylypivyana <yana.pylypiv.ik.2024@lpnu.ua>
2024-10-04 21:19:34 +0300

    added Ut1(5.0)

---
+++ lan_05.1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lan_05.1/lan_05.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double a = 1.0;
+			double b = 2.0;
+			double c = 3.0;
+
+			double expected = a * sin(b) + b * sin(a) + pow(c, 2);
+
+			Assert::AreEqual(expected, f(a, b, c), 0.001);
+		}
+	};
+}
